d4b62262c5534c167005e2281b5e90510721bc17
albertgoncalves <goncalves.albert@gmail.com>
2025-04-12 18:20:14 -0400

    expand example, link with mold

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,6 +5,6 @@ set -eux
 mkdir -p bin/
 
 nasm -felf64 -Fdwarf src/main.s -o bin/main.o
-clang -no-pie bin/main.o -o bin/main
+mold -o bin/main -L "/usr/lib/" -lc -dynamic-linker "/usr/lib64/ld-linux-x86-64.so.2" bin/main.o
 objdump -M intel -l -d bin/main
 ./bin/main || coredumpctl debug -A '-q --readnow -ex "thread apply all backtrace full" -ex q'
--- src/main.s
+++ src/main.s
@@ -1,24 +1,39 @@
-global main
-extern puts
+global _start
+
 extern abort
+extern _exit
+extern printf
+extern puts
 
 section .rodata
-    message db "Hello, world!", 10, 0
+    message db "Hello, world!", 0
+    format  db "%d, %u", 10, 0
+
+section .bss
+    object resq 2
 
 section .text
 
 %line 10+0 "/path/to/foo.bar.main"
-    main:
-        mov     rdi, 123
+    _start:
         call    foo
 
-        mov     rdi, message
-        call    puts
+        mov     qword [object], -456
+        mov     qword [object + 8], 123
 
+        mov     rdx, [object + 8]
+        mov     rsi, [object]
+        mov     rdi, format
         xor     eax, eax
-        ret
+        call    printf
+
+        xor     edi, edi
+        call    _exit
 
 %line 45+0 "/path/to/foo.bar.foo"
     foo:
+        mov     rdi, message
+        call    puts
+
         call    abort
         ret
